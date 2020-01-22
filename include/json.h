3782099bbbcbaebae879c2f5e28a583d3cb81e69
DenisSlack <47750032+DenisSlack@users.noreply.github.com>
2020-01-22 16:34:01 +0300

    Update json.h

--- include/json.h
+++ include/json.h
@@ -1,58 +1,37 @@
-// Copyright 2020 Stitch
-#pragma once
+// Copyright 2019  Dimontich
 
+#ifndef JSON_PARSER_JSON_H
+#define JSON_PARSER_JSON_H
+
+#include <iostream>
+#include <fstream>
 #include <any>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
+#include <unordered_map>
+#include <exception>
 
-class Json {
-  std::any data;
-
- public:
-  // Конструктор из строки, содержащей Json-данные.
-  Json(const std::string &s);
-
-  // Конструктор из строки, содержащей вектор токенов
-  Json(const std::vector<std::any> &tokens);
-
-  // Метод возвращает true, если данный экземпляр содержит в себе JSON-массив.
-  // Иначе false.
-
-  bool is_array() const;
-
-  // Метод возвращает true, если данный экземпляр содержит в себе JSON-объект.
-  // Иначе false.
-
-  bool is_object() const;
-
-  // Метод возвращает значение по ключу key, если экземпляр является
-  // JSON-объектом.
-
-  // Значение может иметь один из следующих типов: Json, std::string, double,
-  // bool или быть пустым.
-
-  // Если экземпляр является JSON-массивом, генерируется исключение.
-
-  std::any operator[](const std::string &key);
-
-  // Метод возвращает значение по индексу index, если экземпляр является
-  // JSON-массивом.
-
-  // Значение может иметь один из следующих типов: Json, std::string, double,
-  // bool или быть пустым.
-
-  // Если экземпляр является JSON-объектом, генерируется исключение.
-
-  std::any operator[](int index);
-
-  // Метод возвращает объект класса Json из строки, содержащей Json-данные.
-
-  static Json parse(const std::string &s);
-
-  // Метод возвращает объекта класса Json из файла, содержащего Json-данные в
-  // текстовом формате.
-
-  static Json parseFile(const std::string &path_to_file);
+class Json
+{
+private:
+    std::any json_map;
+    static void check_str(const std::string& s);
+    static void check_whitespace(const std::string& s);
+    static int get_int(const std::string& s);
+    static double get_double(const std::string& s);
+    static bool get_bool(const std::string& s, int& b);
+    static int find_end(const std::string& s, char close);
+    void parse_object(const std::string& s, int& a, int& b);
+    void parse_array(const std::string& s, int& a, int& b);
+public:
+    Json(const std::string& s);
+    bool is_array() const;
+    bool is_object() const;
+    std::any operator[](const std::string& key);
+    std::any operator[](int index);
+    static Json parse(const std::string& s);
+    static Json parseFile(const std::string& path_to_file);
 };
+
+
+#endif //JSON_PARSER_JSON_H
